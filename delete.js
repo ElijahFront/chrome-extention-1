80ea93f604baece727b35ecade104e09c34dcc57
Ilya <cuprum19930@gmail.com>
2016-12-01 22:10:45 +0500

    deploy version

--- delete.js
+++ delete.js
@@ -1,14 +1,23 @@
+// Author - Ilya Smetanin, cuprum19930@gmail.com
+popup = {
+  number:0
+};
 function deletePopUps(){
     var divs = document.getElementsByTagName('div');
 
     var divsArr = Array.prototype.slice.call(divs, 0);
 
+
     divsArr.forEach(div => {
         if (div.style.border == '1px solid rgb(255, 209, 25)'){
-        div.parentNode.removeChild(div)
+          popup.number++;
+          div.parentNode.removeChild(div)
     }
 });
-    console.log('Removing popups');
+    chrome.runtime.sendMessage(popup.number, function () {
+      console.log(`Removed ${popup.number} elem`);
+    })
+
 }
 
 setInterval(deletePopUps, 2000);
--- popup.js
+++ popup.js
@@ -1,1 +1,7 @@
+// Author - Ilya Smetanin, cuprum19930@gmail.com
 
+var indicator = document.getElementById('counter');
+
+chrome.runtime.onMessage.addListener(function(message) {
+  indicator.innerHTML = 'Убрано: '+ message;
+})
